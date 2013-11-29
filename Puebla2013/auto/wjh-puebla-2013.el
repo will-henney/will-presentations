32d9c2c4c59f10fa427bbef26750aa13d126eb20
William Henney <will@henney.org>
2013-11-29 13:12:13 -0600

    Convert to multimedia package with buttons
    Use buttons to jump around in the movie

---
+++ Puebla2013/auto/wjh-puebla-2013.el
@@ -0,0 +1,31 @@
+(TeX-add-style-hook "wjh-puebla-2013"
+ (lambda ()
+    (TeX-add-symbols
+     "backskip"
+     "backsmallskip"
+     "backmedskip"
+     "backbigskip"
+     "MovieFile"
+     "MoviePlayer")
+    (TeX-run-style-hooks
+     "xcolor"
+     "will-beamer-puebla"
+     "tikz"
+     "pgfpages"
+     "etex"
+     "media9"
+     "booktabs"
+     "siunitx"
+     "hyperref"
+     "textcomp"
+     "fixltx2e"
+     ""
+     "fontenc"
+     "T1"
+     "inputenc"
+     "utf8"
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "presentation")))
+
